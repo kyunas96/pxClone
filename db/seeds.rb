2aa1c420192c5ae68e204e3de689b0ef7fb0c9d2
Kevin Yunas <kevinyunas@icloud.com>
2021-08-02 10:44:24 -0700

    Updated seeds to include user_photo, banner_image, and new photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,52 +9,7 @@
 User.destroy_all
 Post.destroy_all
 
-# 1: Kevin
-User.create!(
-  username: "Kevin", 
-  email: "kevinyunas@icloud.com", 
-  password: "myPassword",
-  country: "United States",
-  city: "Los Angeles",
-  description: 'When I\'m not programming I like running around LA and '\
-  'catching the golden hour light or the breeze at the beach :)'
-)
-# 2: Daniel
-User.create!(
-  username: "Daniel", 
-  email: "daniel@email.com", 
-  password: "danielPassword",
-  country: "United States",
-  city: "Los Angeles",
-  description: 'Love film but it got too expensive :( so here are some '\
-  'digital photo dumps'
-)
-# 3: Gabby
-User.create!(
-  username: "Gabby", 
-  email: "gabby@email.com", 
-  password: "gabbyPassword",
-  country: "United States",
-  city: "Los Angeles",
-  description: "Picked up a camera in Davis, CA and haven't stopped since"
-)
-#4 :Kriste
-User.create!(
-  username: "Kriste", 
-  email: "kriste@email.com", 
-  password: "kristePassword",
-  country: "United States",
-  city: "Los Angeles",
-  description: "When I'm not making music I enjoy catching a couple snapshots"
-)
-#5 :Demo
-User.create!(
-  username: "demoUser", 
-  email: "demo@email.com", 
-  password: "password",
-  country: "United States",
-  city: "Los Angeles"
-)
+# add user_photo and banner_image to each user
 
 daniel1 = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/daniel1.jpeg")
 daniel2 = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/daniel2.jpeg")
@@ -77,6 +32,85 @@ topanga2 = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images
 torn = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/torn.jpeg")
 tunnel = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/tunnel.jpeg")
 
+beach = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/beach.jpeg")
+canyon = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/canyon.jpeg")
+murica = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/murica.jpeg")
+tracks = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/tracks.jpeg")
+surfsup = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/surfsup.jpeg")
+monk = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/monk.jpeg")
+nosotros = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/nosotros.jpeg")
+hashtag = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/hashtag.jpeg")
+grafitti = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/graffiti.jpeg")
+ninguno = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/ningunos.jpeg")
+trippy = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/trippy.jpeg")
+trisha = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/trisha.jpeg")
+lies = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/lies.jpeg")
+stairs = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/stairs.jpeg")
+daniel10 = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/daniel10.jpeg")
+mardigras = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/mardigras.jpeg")
+soak = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/soak.jpeg")
+hangloose = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/hangloose.jpeg")
+sunset = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/sunset.jpeg")
+daniel5 = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/daniel5.jpeg")
+
+kevinProfile = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/portrait.jpg")
+kevinBanner = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/graffiti.jpeg")
+
+danielProfile = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/daniel2.jpeg")
+danielBanner = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/sd.jpeg")
+
+gabbyProfile = open("https://s3.console.aws.amazon.com/s3/object/pxclone-seeds?region=us-west-1&prefix=pxclone-images-compressed/gabs.JPG")
+gabbyBanner = open("https://pxclone-seeds.s3.us-west-1.amazonaws.com/pxclone-images-compressed/davis2.jpeg")
+
+
+
+# 1: Kevin
+kevin = User.create!(
+  username: "Kevin", 
+  email: "kevinyunas@icloud.com", 
+  password: "myPassword",
+  country: "United States",
+  city: "Los Angeles",
+  description: 'When I\'m not programming I like running around LA and '\
+  'catching the golden hour light or the breeze at the beach :)'
+)
+
+kevin.user_photo.attach(io: kevinProfile, filename: "kevinProfile.jpg")
+kevin.banner_image.attach(io: kevinBanner, filename: "kevinBanner.jpg")
+# 2: Daniel
+daniel = User.create!(
+  username: "Daniel", 
+  email: "daniel@email.com", 
+  password: "danielPassword",
+  country: "United States",
+  city: "Los Angeles",
+  description: 'Love film but it got too expensive :( so here are some '\
+  'digital photo dumps'
+)
+
+daniel.user_photo.attach(io: danielProfile, filename: "danielProfile.jpg")
+daniel.banner_image.attach(io: danielBanner, filename: "danielBanner.jpg")
+# 3: Gabby
+gabby_user = User.create!(
+  username: "Gabby", 
+  email: "gabby@email.com", 
+  password: "gabbyPassword",
+  country: "United States",
+  city: "Los Angeles",
+  description: "Picked up a camera in Davis, CA and haven't stopped since"
+)
+
+gabby_user.user_photo.attach(io: gabbyProfile, filename: "gabbyProfile.jpg")
+gabby_user.banner_image.attach(io: gabbyBanner, filename: "gabbyBanner.jpg")
+#5 :Demo
+User.create!(
+  username: "demoUser", 
+  email: "demo@email.com", 
+  password: "password",
+  country: "United States",
+  city: "Los Angeles"
+)
+
 daniel1Post = Post.new(title: "Beach B/W", poster_id: 2, description: "")
 daniel2Post = Post.new(title: "Cheesin in Davis", poster_id: 2, description: "")
 daniel3Post = Post.new(title: "Beach flying", poster_id: 2, description: "")
@@ -87,7 +121,7 @@ dazedPost = Post.new(title: "Woah", poster_id: 2, description: "")
 gabbyPost = Post.new(title: "Supreme Queen", poster_id: 3, description: "")
 gpaPost = Post.new(title: "Yikes", poster_id: 2, description: "")
 griffithPost = Post.new(title: "Thats a nice wallpaper", poster_id: 1, description: "")
-kristePost = Post.new(title: "Ayyyyy", poster_id: 4, description: "")
+kristePost = Post.new(title: "Ayyyyy", poster_id: 1, description: "")
 littletokyoPost = Post.new(title: "City scapes", poster_id: 1, description: "")
 palacePost = Post.new(title: "Woah", poster_id: 1, description: "")
 sdPost = Post.new(title: "The water's fine", poster_id: 2, description: "")
@@ -97,6 +131,48 @@ topanga1Post = Post.new(title: "You put the lime in the coconut", poster_id: 1, 
 topanga2Post = Post.new(title: "Mild mannered postcard", poster_id: 1, description: "")
 tornPost = Post.new(title: "Torn", poster_id: 1, description: "")
 tunnelPost = Post.new(title: "Ok so you're going to walk through this tunnel...", poster_id: 2, description: "")
+
+beachPost = Post.new(title: "Summer Breeze", poster_id: 1, description: "")
+canyonPost = Post.new(title: "Canyon", poster_id: 3, description: "")
+muricaPost = Post.new(title: "Peekaboo", poster_id: 1, description: "")
+tracksPost = Post.new(title: "To where", poster_id: 1, description: "")
+surfsupPost = Post.new(title: "Surfs up", poster_id: 3, description: "")
+monkPost = Post.new(title: "In another life", poster_id: 1, description: "")
+nosotrosPost = Post.new(title: "Nosotros", poster_id: 3, description: "")
+hashtagPost = Post.new(title: "Hashtag", poster_id: 3, description: "")
+grafittiPost = Post.new(title: "Fine print", poster_id: 3, description: "")
+ningunoPost = Post.new(title: "Ninguno", poster_id: 3, description: "")
+trippyPost = Post.new(title: "Cherry-coloured Funk", poster_id: 1, description: "")
+trishaPost = Post.new(title: "Trisha", poster_id: 2, description: "")
+liesPost = Post.new(title: "Makes you wonder", poster_id: 3, description: "")
+stairsPost = Post.new(title: "Where do they lead", poster_id: 3, description: "")
+daniel10Post = Post.new(title: "Lousiana", poster_id: 2, description: "")
+mardigrasPost = Post.new(title: "Mardigras awaits", poster_id: 2, description: "")
+soakPost = Post.new(title: "Soakin in the sun", poster_id: 2, description: "")
+hangloosePost = Post.new(title: "Hangloose", poster_id: 3, description: "")
+sunsetPost = Post.new(title: "Almost golden hour", poster_id: 2, description: "")
+daniel5Post = Post.new(title: "Stay awhile", poster_id: 2, description: "")
+
+beachPost.photo.attach(io: beach, filename: "beach.jpg")
+canyonPost.photo.attach(io: canyon, filename: "canyon.jpg")
+muricaPost.photo.attach(io: murica, filename: "murica.jpg")
+tracksPost.photo.attach(io: tracks, filename: "tracks.jpg")
+surfsupPost.photo.attach(io: surfsup, filename: "surfsup.jpg")
+monkPost.photo.attach(io: monk, filename: "monk.jpg")
+nosotrosPost.photo.attach(io: nosotros, filename: "nosotros.jpg")
+hashtagPost.photo.attach(io: hashtag, filename: "hashtag.jpg")
+grafittiPost.photo.attach(io: grafitti, filename: "grafitti.jpg")
+ningunoPost.photo.attach(io: ninguno, filename: "ninguno.jpg")
+trippyPost.photo.attach(io: trippy, filename: "trippy.jpg")
+trishaPost.photo.attach(io: trisha, filename: "trisha.jpg")
+liesPost.photo.attach(io: lies, filename: "lies.jpg")
+stairsPost.photo.attach(io: stairs, filename: "stairs.jpg")
+daniel10Post.photo.attach(io: daniel10, filename: "daniel10.jpg")
+mardigrasPost.photo.attach(io: mardigras, filename: "mardigras.jpg")
+soakPost.photo.attach(io: soak, filename: "soak.jpg")
+hangloosePost.photo.attach(io: hangloose, filename: "hangloose.jpg")
+sunsetPost.photo.attach(io: sunset, filename: "sunset.jpg")
+daniel5Post.photo.attach(io: daniel5, filename: "daniel5.jpg")
 
 daniel1Post.photo.attach(io: daniel1, filename: "daniel1.jpg")
 daniel2Post.photo.attach(io: daniel2, filename: "daniel2.jpg")
@@ -137,3 +213,24 @@ topanga1Post.save!
 topanga2Post.save!
 tornPost.save!
 tunnelPost.save!
+
+beachPost.save!
+canyonPost.save!
+muricaPost.save!
+tracksPost.save!
+surfsupPost.save!
+monkPost.save!
+nosotrosPost.save!
+hashtagPost.save!
+grafittiPost.save!
+ningunoPost.save!
+trippyPost.save!
+trishaPost.save!
+liesPost.save!
+stairsPost.save!
+daniel10Post.save!
+mardigrasPost.save!
+soakPost.save!
+hangloosePost.save!
+sunsetPost.save!
+daniel5Post.save!
