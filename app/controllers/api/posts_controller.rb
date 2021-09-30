b638a09f659968aa02482347519718f998692c8e
Kevin Yunas <kevinyunas@icloud.com>
2021-09-30 14:10:35 -0700

    Refactored posts_controller#index to leverage User.followings association

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,7 +2,7 @@ class Api::PostsController < ApplicationController
 
   def index 
     # refactor 
-    @followed_users_id = Follow.where(follower: current_user.id).map(&:followed_user_id)
+    @followed_users_id = current_user.followings.map(&:id)
     @liked_posts_ids = current_user.likes.map(&:post_id)
     @posts = Post.where(:poster_id => @followed_users_id).order(:created_at)
 
